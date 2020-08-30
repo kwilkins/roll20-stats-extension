0adb9565caa501c4f00ea9eb062a438ff420a796
Kevin Wilkinson <padfoot240@gmail.com>
2020-08-30 11:24:14 -0700

    added parsing logic started adding react component view

--- public/roll20_chat_parser.js
+++ public/roll20_chat_parser.js
@@ -4,8 +4,72 @@ var response = {
 
 console.log('Running parser...');
 
-if ($('#textchat .content').length) {
-  response.data = 'Roll20 chat was found!';
+if ($('#textchat > .content').length) {
+  // if we find a roll20 chat, gather data
+  var lastMessageBy = '';
+  var parsedRoll20Data = {
+    playerNames: [],
+    d20Rolls: [],
+    // only d20 scores for now...
+    // damage rolls?
+    // 2d6?
+  };
+  
+  var processMessage = (index, element) => {
+    // function that processes every message in roll20 chat
+    const messageElement = $(element);
+    console.log('processing message...', element);
+    let storePlayer = false;
+
+    if (messageElement.children('.by').length) {
+      // if this message has a player name associated with it, we keep it
+      // for repeat messages
+      lastMessageBy = messageElement.children('.by').text().replace(':', '');
+    }
+
+    if (messageElement.hasClass('rollresult')) {
+      // This message is a standard roll20 dice roll
+      messageElement.find('.diceroll.d20').each((index, element) => {
+        const rollResult = $(element).find('.didroll').text();
+
+        parsedRoll20Data.d20Rolls = [...parsedRoll20Data.d20Rolls, { roller: lastMessageBy, result: rollResult }];
+        storePlayer = true;
+      });
+    }
+
+    if (messageElement.find('div[class^=\'sheet-rolltemplate\']').length > 0) {
+      // This message contains a templated roll from a Roll20 character sheet.
+      // For whatever reason the templated rolls use an html element attribute
+      // named "original-title" which is NOT a valid attribute name, jquery
+      // separates this into two attributes which is why we are searching for
+      // an attribute named "title" instead.
+      messageElement.find('span.inlinerollresult').each((index, element) => {
+        const rollElement = $(element);
+
+        const inlineRoll = rollElement.attr('title') ? rollElement.attr('title') : rollElement.attr('original-title');
+        const rollResult = $('<div/>').html(inlineRoll).find('.basicdiceroll').text();
+
+        console.log('inlineRoll: ', inlineRoll);
+        if (inlineRoll.indexOf('Rolling 1d20') > -1) {
+          parsedRoll20Data.d20Rolls = [...parsedRoll20Data.d20Rolls, { roller: lastMessageBy, result: rollResult }];
+          storePlayer = true;
+        }
+      });
+    }
+
+    if (storePlayer && !parsedRoll20Data.playerNames.find((element) => element === lastMessageBy)) {
+      // if we recorded a roll for this player, and they are not already in our data, add it to playerNames
+      parsedRoll20Data.playerNames = [...parsedRoll20Data.playerNames, lastMessageBy];
+    }
+  }
+
+  $('#textchat > .content').children('.message').each(processMessage);
+
+  if (parsedRoll20Data.playerNames.length) {
+    response.data = parsedRoll20Data;
+	} else {
+    response.error = 'No player messages were found in the chat';
+	}
 } else {
   response.error = 'You need to be on a page with a roll20 chat in order to use this extension';
 }
--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-import logo from './logo.svg';
-
 const App = () => {
   const [message, setMessage] = useState('Nothing yet...');
+  const [statData, setStatData] = useState(null);
   useEffect(() => {
     window.chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-      console.log(message);
-      console.log(message.type);
-      console.log(message.type === 'ROLL20_STATS_EXTENSION');
       if (message.type === 'ROLL20_STATS_EXTENSION') {
-        if (message.error) {
-          setMessage(message.error);
-        } else {
-          setMessage(message.data);
-        }
+        setMessage(message);
       }
     });
 
@@ -32,22 +24,46 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {message.error &&
+        <span>{message.error}</span>
+      }
+      {message.data &&
+        <>
+          <div className="parsed-results">
+            <div>Found rolls for {message.data.playerNames.length} players.</div>
+            {renderPlayerInputs(message.data.playerNames)}
+            <button className="calculate-stats" onClick={() => calculateStats(setStatData)}>Calculate Stats!</button>
+          </div>
+          <div className="player-list">
+            {statData &&
+              'Player Stats Here!'
+            }
+          </div>
+        </>
+      }
     </div>
   );
+};
+
+const renderPlayerInputs = (playerNames) => {
+  return (
+    playerNames.map((name) => {
+      return (
+        <div>
+          <input id={`alias-${name}`} placeholder={name} />
+          {name}
+        </div>
+      )
+    })
+  );
+};
+
+const calculateStats = (setStatData) => {
+  setStatData(null);
+
+  // actually calculate the stats...
+
+  setStatData({ enabled: true });
 }
 
 export default App;
