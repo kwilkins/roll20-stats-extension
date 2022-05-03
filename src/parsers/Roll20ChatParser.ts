fd70042e80df2b45672d526ab887b1720ecb1a38
kwilkins <kevin@kwilkins.com>
2022-05-02 17:01:40 -0700

    refactored static strings as properties

--- src/parsers/Roll20ChatParser.ts
+++ src/parsers/Roll20ChatParser.ts
@@ -1,15 +1,31 @@
 import { IRollData, IRollRecord, RollDataDiceRollsPropertyName } from '../model/DiceRollInterfaces';
 
+/**
+ * @description Class responsible for parsing a Roll20 chat DOM.
+ */
 export default class Roll20ChatParser {
+  // CSS Selectors
+  private static readonly messageClassDirectDescendantOfContentClassSelector: string = '.content > .message';
+  private static readonly byClassSelector: string = '.by';
+  private static readonly divWithSheetRollTemplateClassSelector: string = 'div[class^=\'sheet-rolltemplate\']';
+  private static readonly diceRollAndD20ClassSelector: string = '.diceroll.d20';
+  private static readonly spanWithInlineRollResultClassSelector: string = 'span.inlinerollresult';
+  private static readonly basicDiceRollClassSelector: string = '.basicdiceroll';
+  // Class names and attribute values
+  private static readonly rollResultClassValue: string = 'rollresult';
+  private static readonly titleAttributeValue: string = 'title';
+  // Text values
+  private static readonly inlineRolling1D20TextValue: string = 'Rolling 1d20';
+
   private lastParsedMessageSentBy: string;
   private parsedRollerNames: Set<string>;
   private parsedRollRecords: Record<RollDataDiceRollsPropertyName, IRollRecord[]>;
 
   /**
-   * @constructor Creates a new Roll20 Chat Parser object.
+   * @constructor Creates a new Roll20 chat parser object.
    */
   public constructor() {
-    this.lastParsedMessageSentBy = '';
+    this.lastParsedMessageSentBy = 'UNKNOWN ROLLER';
     this.parsedRollerNames = new Set<string>();
     this.parsedRollRecords = {
       [RollDataDiceRollsPropertyName.d20]: []
@@ -32,7 +48,7 @@ export default class Roll20ChatParser {
     console.log('properties initialized...');
 
     try {
-      const chatMessages = roll20ChatDOM.querySelectorAll('.content > .message');
+      const chatMessages = roll20ChatDOM.querySelectorAll(Roll20ChatParser.messageClassDirectDescendantOfContentClassSelector);
       chatMessages.forEach(this.processMessage);
 
       if (this.parsedRollerNames.size) {
@@ -56,7 +72,7 @@ export default class Roll20ChatParser {
   };
 
   private processMessage = (messageElement: Element, key: number): void => {
-    const messageSenderText = messageElement.querySelector('.by')?.textContent;
+    const messageSenderText = messageElement.querySelector(Roll20ChatParser.byClassSelector)?.textContent;
     if (messageSenderText) {
       // if this message has a player name associated with it, we keep it
       // for 'stacked' messages that only show the player name on the first
@@ -64,9 +80,9 @@ export default class Roll20ChatParser {
       this.lastParsedMessageSentBy = messageSenderText?.substring(0, messageSenderText.lastIndexOf(':')).trim();
     }
 
-    if (messageElement.className.includes('rollresult')) {
+    if (messageElement.className.includes(Roll20ChatParser.rollResultClassValue)) {
       this.handleStandardRoll(messageElement);
-    } else if (messageElement.querySelector('div[class^=\'sheet-rolltemplate\']')) {
+    } else if (messageElement.querySelector(Roll20ChatParser.divWithSheetRollTemplateClassSelector)) {
       this.handleRoll20CharacterSheetTemplateRoll(messageElement);
     } else if (false) {
       this.handleBeyond20ExtensionTemplateRoll(messageElement);
@@ -75,10 +91,10 @@ export default class Roll20ChatParser {
 
   private handleStandardRoll = (messageElement: Element): void => {
     // This message is a standard roll20 d20 dice roll
-    const standardD20RollSelector = messageElement.querySelectorAll('.diceroll.d20');
+    const standardD20RollSelector = messageElement.querySelectorAll(Roll20ChatParser.diceRollAndD20ClassSelector);
     if (standardD20RollSelector.length) {
       standardD20RollSelector.forEach((standardD20DiceRollElement: Element, key: number) => {
-        const rollResult = standardD20DiceRollElement.querySelector('.didroll')?.textContent;
+        const rollResult = standardD20DiceRollElement.querySelector(Roll20ChatParser.diceRollAndD20ClassSelector)?.textContent;
 
         if (rollResult) {
           this.addD20RollResult(this.lastParsedMessageSentBy, rollResult);
@@ -91,17 +107,17 @@ export default class Roll20ChatParser {
   // When the sheet is set to 'Advantage Toggle' the 'ignored' value is contained by .sheet-grey
   // When the sheet is set to 'Always Roll Advantage' it just rolls two values
   private handleRoll20CharacterSheetTemplateRoll = (messageElement: Element): void => {
-    const inlineRollSelector = messageElement.querySelectorAll('span.inlinerollresult');
+    const inlineRollSelector = messageElement.querySelectorAll(Roll20ChatParser.spanWithInlineRollResultClassSelector);
     if (inlineRollSelector.length) {
       inlineRollSelector.forEach((inlineD20DiceRollElement: Element, key: number) => {
         // For whatever reason the inline rolls use an html element attribute
         // named "original-title" which is NOT a valid attribute name, proper
         // browser parsing separates this into two attributes which leaves title
         // as the one we want to look for.
-        const inlineRollTitleAttributeString = inlineD20DiceRollElement.getAttribute('title');
-        if (inlineRollTitleAttributeString?.includes('Rolling 1d20')) {
+        const inlineRollTitleAttributeString = inlineD20DiceRollElement.getAttribute(Roll20ChatParser.titleAttributeValue);
+        if (inlineRollTitleAttributeString?.includes(Roll20ChatParser.inlineRolling1D20TextValue)) {
           const inlineRollAsDoc = new DOMParser().parseFromString(`<div>${inlineRollTitleAttributeString}</div>`, 'text/html');
-          const rollResult = inlineRollAsDoc.querySelector('.basicdiceroll')?.textContent;
+          const rollResult = inlineRollAsDoc.querySelector(Roll20ChatParser.basicDiceRollClassSelector)?.textContent;
           if (rollResult) {
             this.addD20RollResult(this.lastParsedMessageSentBy, rollResult);
           }
